aeb53ea4939ab1cc1a6d3ea76b4c3b289ff7e3e3
Jerry Quintero <jerryq27@gmail.com>
2022-07-14 00:02:20 -0500

    Displaying a sample list of todos.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TodoForm from './components/TodoForm';
 
 function App() {
+  let [todos, setTodos] = useState<Array<string>>([
+    'Learn React',
+    'Create Todo app',
+    'Learn TypeScript',
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {todos.map((todo: string) => {
+          return <li key={todo}>{todo}</li>
+        })}
+      </ul>
+      <TodoForm />
     </div>
   );
 }
