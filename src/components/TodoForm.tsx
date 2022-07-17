a87dc7e68363e82e038da043b883c436992d2dcf
Jerry Quintero <jerryq27@gmail.com>
2022-07-17 04:24:21 -0500

    New todos are being added.

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const TodoForm = () => {
+    const { addTodo } = useContext(AppContext);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        // Prevent the browser from refreshing.
+        event.preventDefault();
+
+        let newTodo = (document.getElementById("todo-text") as HTMLInputElement).value;
+        if (newTodo) {
+            addTodo({ todo: newTodo });
+            // Clear the text field.
+            (document.getElementById("todo-text") as HTMLInputElement).value = "";
+        }
+        else {
+            alert("Must submit a value.");
+        }
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="todo-text">Todo</label>
             </div>
             <div>
                 <input id="todo-text" name="todo-text" type="text" />
             </div>
-            <button type="button">Add Todo</button>
+            <button>Add Todo</button>
         </form>
     );
 }
