91fed9a05b47aaa059686a28a64f861bb0eeeda0
Jerry Quintero <jerryq27@gmail.com>
2022-07-23 00:06:21 -0500

    Updated context.
    Added delete and update methods and implemented them. Also added IDs to the todos.

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -7,8 +7,11 @@ import React, {
 export interface AppContextType {
     todos: TodoType[];
     addTodo: (todo: TodoType) => void;
+    deleteTodo: (todo: TodoType) => void;
+    updateTodo: (todo: TodoType) => void;
 }
 export interface TodoType {
+    id: number;
     todo: string;
 }
 
@@ -16,17 +19,32 @@ export const AppContext = createContext<AppContextType>({} as AppContextType);
 
 export const AppContextProvider: React.FC<PropsWithChildren> = ({ children }) => {
     const [todos, setTodos] = useState<TodoType[]>([
-        { todo: "Learn React" },
-        { todo: "Create Todo app" },
-        { todo: "Learn TypeScript" },
+        { id: 1, todo: "Learn React" },
+        { id: 2, todo: "Create Todo app" },
+        { id: 3, todo: "Learn TypeScript" },
     ]);
 
     const addTodo = (todo: TodoType) => {
         setTodos([...todos, todo]);
     }
 
+    const deleteTodo = (todo: TodoType) => {
+        setTodos(todos.filter(t => t.id !== todo.id));
+    }
+
+    const updateTodo = (todo: TodoType) => {
+        let newTodos = [...todos];
+        newTodos.map(t => {
+            if (t.id == todo.id) {
+                t = todo;
+            }
+        });
+
+        setTodos(newTodos);
+    }
+
     return (
-        <AppContext.Provider value={{ todos, addTodo }}>
+        <AppContext.Provider value={{ todos, addTodo, deleteTodo, updateTodo }}>
             {children}
         </AppContext.Provider>
     );
